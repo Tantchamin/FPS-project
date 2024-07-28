9242774349d163298e838e396053f57d6323cab7
Tantchamin <78613005+Tantchamin@users.noreply.github.com>
2024-07-28 23:07:49 +0700

    Update: map update, update gravity apply to player, update player move control

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,11 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] private CharacterController controller;
     [SerializeField] private float moveSpeed = 1f;
-    [SerializeField] private Transform orientation;
     private Rigidbody playerRigidbody;
     private Vector3 moveDirection;
+
+    [Header("GroundCheck")]
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundMask;
+    [SerializeField] private float groundDistance = 0.3f;
+    private const float gravity = -9.8f;
+    private Vector3 playerVelocity;
+    bool isGrounded = false;
 
     void Start()
     {
@@ -17,12 +25,18 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        CharacterGravity();
         MoveCharacter();
 
     }
 
-    void FixedUpdate()
+    private void CharacterGravity()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if (isGrounded && playerVelocity.y < 0)
+        {
+            playerVelocity.y = -2f;
+        }
     }
 
     void MoveCharacter()
@@ -30,12 +44,11 @@ public class PlayerController : MonoBehaviour
         float xInput = Input.GetAxis("Horizontal");
         float zInput = Input.GetAxis("Vertical");
 
-        //moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
-        //playerRigidbody.MovePosition(transform.position + moveDirection * moveSpeed * Time.deltaTime);
-
         moveDirection = transform.right * xInput + transform.forward * zInput;
         controller.Move(moveDirection * moveSpeed * Time.deltaTime);
 
+        playerVelocity.y += gravity * Time.deltaTime;
 
+        controller.Move(playerVelocity);
     }
 }
